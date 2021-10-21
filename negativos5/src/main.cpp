2de8f453818ffee6be3059698e7c1ad22461705b
alan castro <castro_alan11@hotmail.com>
2021-10-21 16:38:12 -0300

    ResoluçãoNegativos

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
-using std::cin;
 using std::cout;
+using std::cin;
+using std::endl;
 
 const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-
+    int num, neg = 0;
+    for (int i = 0; i < SIZE; i++) {
+        std::cin >> num;
+        if (num < 0) {
+            neg++;
+        }
+    }
+    std::cout << neg;
     return 0;
 }
