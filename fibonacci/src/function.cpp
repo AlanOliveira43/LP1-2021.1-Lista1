96471bb7b0c7c45a75e29276cead0c4986be5c2e
alan castro <castro_alan11@hotmail.com>
2021-11-04 22:06:25 -0300

    v1

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,9 +1,38 @@
-#include "function.h"
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
 
-std::vector<unsigned int> fib_below_n( unsigned int n )
+std::vector<int> fib_below_n(unsigned int n) {
+    std::vector<int> meuvetor;
+    int test = 0;
+    for (int i = 0;i < n;i++) {
+        if (i == 0 || i == 1) {
+            meuvetor.push_back(1);
+        }
+        else {
+            test = meuvetor[meuvetor.size() - 1] + meuvetor[meuvetor.size() - 2];
+            if (test < n) {
+                meuvetor.push_back(test);
+            }
+            else {
+                break;
+            }
+        }
+    }
+    return meuvetor;
+}
+
+int main()
 {
-    // TODO: adicione o seu código aqui.
-
-    // TODO: Isto é apenas um STUB. Troque o retorno pelo que você julgar correto.
-    return std::vector<unsigned int>{};
+    int n;
+    std::vector<int> meuvetor;  
+    std::cin >> n;
+    meuvetor = fib_below_n(n);
+    for (int i = 0; i < meuvetor.size(); i++) {
+        std::cout << meuvetor[i] << "\n";
+    }
+    return 0;
 }
